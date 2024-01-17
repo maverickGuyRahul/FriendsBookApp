7431d4f56da9639de26ba38f822f111a04d9a654
Romeo Alpha <rahul.t2001@gmail.com>
2024-01-17 11:25:41 +0530

    everything working perfectly

---
+++ myproject/routing.py
@@ -0,0 +1,14 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+from django.core.asgi import get_asgi_application
+
+
+application = ProtocolTypeRouter({
+    # "http": get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
